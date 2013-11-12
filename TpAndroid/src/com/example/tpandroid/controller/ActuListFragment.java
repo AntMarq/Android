6bbb2f5811ebbb4de518af2355c40c36532d841c
AntMarq <anthonymarquet21@gmail.com>
2013-11-12 16:33:53 +0100

    test

--- TpAndroid/src/com/example/tpandroid/controller/ActuListFragment.java
+++ TpAndroid/src/com/example/tpandroid/controller/ActuListFragment.java
@@ -1,6 +1,7 @@
 package com.example.tpandroid.controller;
 
 import com.example.tpandroid.R;
+
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ import com.example.tpandroid.GlobalApplication;
 import com.example.tpandroid.loader.ActuLoadRss;
 import com.example.tpandroid.model.ObjActuRss;
 import com.example.tpandroid.view.ActuRssAdapter;
+import com.markupartist.android.widget.PullToRefreshListView;
 import com.markupartist.android.widget.PullToRefreshListView.OnRefreshListener;
 
 
